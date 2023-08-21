794fb1967d2541465c421a187a52e1818bf8318a
Jelena077 <jelenaasavicc@googlemail.com>
2023-08-21 21:01:56 +0200

    Add project files.

---
+++ evenorodd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenorodd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            if (a % 2 == 0)
+            {
+                    Console.WriteLine("a is even");
+            }
+            else 
+            {
+                    Console.WriteLine("a is odd");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
